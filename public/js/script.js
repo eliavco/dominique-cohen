b50ccba7a22d236822da805d9be6c34954edfc9b
Eliav <eliav.s.cohen@gmail.com>
2019-10-17 19:18:13 +0300

    17/10/2019 7:18PM

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+document.getElementById("close-panel").addEventListener("click", function(e) {
+  e.preventDefault();
+  document.querySelector(".panel").remove("panel--active");
+});
+
+document.querySelectorAll(".btn--art").forEach(function(el) {
+  el.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(".panel").classList.add("panel--active");
+    document.querySelector(".panel__text").textContent = e.target.dataset.desc;
+    document.querySelector(".panel__image").src = e.target.dataset.img;
+  });
+});
